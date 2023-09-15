ef2fe08b13b09db0fc2a089ad695b9f0d8e4682c
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-15 18:36:07 -0500

    added header 2/2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
    <>
-    {/* <Header/>
-    <Hero/>
+    <Header/>
+   {/*  <Hero/>
     <About/>
     <Projects/>
     <Blog/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,14 +11,54 @@ const Header = () => {
       <a href='/' className='logo text-2xl font-bold text-accent'>
         Kushtarbek
       </a>
-
+      {/*Desktop Nav */}
       <nav className='hidden md:block'>
         <ul className='flex'>
           <li>
             <a href='#about'>About</a>
           </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#blog'>Blog</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+          <li>
+            <a href='#resume'>Resume</a>
+          </li>
         </ul>
       </nav>
+
+      <nav
+        className={!toggle ? 'mobile-nav left-[-100%]' : 'mobile-nav left-0'}
+      >
+        <ul className='flex flex-col'>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#blog'>Blog</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+          <li>
+            <a href='#resume'>Resume</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button onClick={handleToggle} className='block md:hidden'>
+        {!toggle ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
+      </button>
     </header>
   );
 };
+
+export default Header;
