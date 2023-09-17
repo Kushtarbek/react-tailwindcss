f6cf0d04192fe6fd87d07ca165541947af8b57c9
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-17 16:25:11 -0500

    projects added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
 import './App.css';
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
    <>
     <Header/>
     <Hero/>
-      {/* <About/>
+    <About/>
     <Projects/>
-    <Blog/>
+    {/*<Blog/>
     <Contact/>
     <Footer/> */}
    </>
