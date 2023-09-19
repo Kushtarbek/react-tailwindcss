ac3420dc31084b08fea932dc07d78d5a38dc23ce
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-19 12:03:01 -0500

    added contacts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Blog from './components/Blog'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
     <About/>
     <Projects/>
    <Blog/>
-    {/* <Contact/>
-    <Footer/> */}
+    <Contact/>
+    {/* <Footer/> */}
    </>
   );
 }
