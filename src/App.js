bca27df58370009e836b0f7822df21dabaac042e
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-19 11:48:39 -0500

    added blog component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
+import Blog from './components/Blog'
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
     <Hero/>
     <About/>
     <Projects/>
-    {/*<Blog/>
-    <Contact/>
+   <Blog/>
+    {/* <Contact/>
     <Footer/> */}
    </>
   );
